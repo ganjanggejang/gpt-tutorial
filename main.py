421fc4bff10d047fda00102bf24dda61791e5403
Junsoo <35479251+ganjanggejang@users.noreply.github.com>
2023-03-26 05:18:46 +0000

    added "more keywords"

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 def index():
     if request.method == "POST":
         input_text = request.form["input_text"]
-        response = openai.ChatCompletion.create(
+        response_summarize = openai.ChatCompletion.create(
           model="gpt-3.5-turbo",
           messages = [
-            {'role': 'user', 'content': generate_prompt(input_text)}
+            {'role': 'user', 'content': generate_prompt_1(input_text)}
           ],
           temperature=0.7,
           max_tokens=64,
@@ -22,20 +22,36 @@ def index():
           frequency_penalty=0.0,
           presence_penalty=0.0
         )
-        return redirect(url_for("index", result=response['choices'][0]['message']['content']))
+        response_keyword = openai.ChatCompletion.create(
+          model="gpt-3.5-turbo",
+          messages = [
+            {'role': 'user', 'content': generate_prompt_2(input_text)}
+          ],
+          temperature=0.7,
+          max_tokens=64,
+          top_p=1.0,
+          frequency_penalty=0.0,
+          presence_penalty=0.0
+        )
+        return redirect(url_for("index", result=response_summarize['choices'][0]['message']['content'], keyword=response_keyword['choices'][0]['message']['content']))
     
     # request.method == "GET"일 때
     result = request.args.get("result")
-    return render_template("index.html", result=result) # html파일에 result 변수도 같이 전달
+    keyword = request.args.get("keyword")
+    return render_template("index.html", result=result, keyword=keyword) # html파일에 result 변수도 같이 전달
 
 
 
   
-def generate_prompt(input_text):
+def generate_prompt_1(input_text):
     return """Summarize this for a high-school student: 
     {}
     """.format(input_text)
 
+def generate_prompt_2(input_text):
+    return """Pick 5 or so difficult keywords that require deeper learning of this text:
+    {}
+    """.format(input_text)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=81)
